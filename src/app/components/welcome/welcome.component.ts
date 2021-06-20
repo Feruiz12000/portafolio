91f03a49028ad943782e8fab4a43d79df3560be5
Fernando Ruiz <feruiz.12000@gmail.com>
2021-06-19 19:51:32 -0600

    img

--- src/app/components/welcome/welcome.component.ts
+++ src/app/components/welcome/welcome.component.ts
@@ -7,6 +7,7 @@ import {Router} from '@angular/router';
 })
 export class WelcomeComponent implements OnInit {
 
+  anio = (new Date).getFullYear();
   constructor(private router:Router) { }
 
   ngOnInit(): void {
